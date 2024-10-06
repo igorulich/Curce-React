95d03b2cec53415bc50bac25bb7a2ff38ef61dd9
Ulich Igor <ylichigor@gmail.com>
2024-10-06 11:52:31 +0300

    setCounter

--- src/App.js
+++ src/App.js
@@ -3,11 +3,19 @@ import Header from './pages/Header/Header';
 import Main from './pages/Main/Main';
 import Sitebar from './pages/Sitabar/Sitebar';
 import Footer from './pages/Footer/Footer';
+import { useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+ 
+  function Increase() {
+    setCounter(counter+1)
+  }
   return (
     <>
-    <div className='counter'>
+      <div className='timeCounter'>
+        <h3>{counter}</h3>
+        <button onClick={Increase}>Click me +</button>
     </div>
     <Header/>
       <Main />
