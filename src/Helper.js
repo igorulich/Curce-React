205dbfbb39e94f9354fdd5b922b0b4601c9bd905
Ulich Igor <ylichigor@gmail.com>
2024-10-12 08:58:54 +0300

    Цель урока – определить, что такое DOM-элементы в React. Ознакомиться с хуком прямого доступа к DOM-элементам useRef. Определить, что такое рендер и как он работает. Ознакомиться с хуком обработки посторонних эффектов

---
+++ src/Helper.js
@@ -0,0 +1,8 @@
+export default function Helper() {
+  return (
+    <>
+      <h1>'Helper function for React application';</h1>
+    
+  </>
+  )
+}
---
+++ src/Test.js
@@ -0,0 +1,9 @@
+function Test() {
+  return (
+    <div className="Test">
+      {console.log('render component Test')}
+      <p>Hello, React!</p>
+    </div>
+  );
+}
+export default Test;
