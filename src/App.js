053b1bb18971147cbdb9f06dd2a6a944637adf0a
Ulich Igor <ylichigor@gmail.com>
2024-10-13 15:24:53 +0300

    хуком прямого доступа к DOM-элементам useRef

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
+import { useRef } from 'react';
 import './App.css'
 import Header from './pages/Header/Header';
 import Main from './pages/Main/Main';
 import Sitebar from './pages/Sitabar/Sitebar';
 import Footer from './pages/Footer/Footer';
-import { useState } from 'react';
+
 
 function App() {
-  const [myState, setMystate] = useState(1);
-
-  function change() {
-    setMystate(myState + 10);
  
-  }
+  const but1 = useRef();
+  
+  function buttonListener(event) {
+  console.log(event.target)
+}
 
   return (
     <>
       <div className='AppTest'>
-        console.log('render component App')
-        <p>My State is: {myState}</p>
-        <button onClick={change}>Change My State</button>
+     
+       
+        <button ref={but1} onClick={buttonListener}>PUSH</button>
+        <button onClick={buttonListener}>ABC</button>
       </div>
     <Header/>
       <Main />
